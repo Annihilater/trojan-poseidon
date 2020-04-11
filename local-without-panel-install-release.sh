161948ebe4007fa9cb78f8dc130bc86e75c5ca00
Colette Contreras <ColetteContreras@outlook.com>
2020-04-12 00:51:06 +0800

    Update

--- local-without-panel-install-release.sh
+++ local-without-panel-install-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEW_VER=v0.0.3
+NEW_VER=v0.0.4
 DOWNLOAD_LINK="https://github.com/ColetteContreras/trojan-poseidon/releases/download/${NEW_VER}/trojanp-linux-64.zip"
 
 INSTALL_DIR=${INSTALL_DIR:-"/root"}
